ca35e645b922bc6da1d10502a2f64390d178542b
Irfan Khan Mohammed <mr.irfankhan196@gmail.com>
2026-08-07 17:14:23 -0500

    added few functions in app

--- applications/01_todo_app.py
+++ applications/01_todo_app.py
@@ -4,9 +4,29 @@ import logging
 
 # variables
 
-
+task = {}
 
 # functions
+
+def gen_task_key():
+    task_key = len(task.keys())
+    return task_key + 1
+
+def create_task(task_val: str):
+    task_key = gen_task_key()
+    task.update({task_key: task_val})
+
+def add_task():
+    pass
+
+def update_task():
+    pass 
+
+def delete_task():
+    pass
+
+def view_tasks():
+    pass
 
 if __name__ == "__main__":
     print("----- ToDo App ------")
@@ -21,3 +41,7 @@ if __name__ == "__main__":
         user_choice = int(input("Enter option: "))
     except Exception as e:
         print(f"Wrong input. Error - {e}")
+
+
+    # test
+    # print(gen_task_key())
